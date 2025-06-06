c61196d1993198c16c606cd697c6362c56ad0ae9
신관규 <soundbar91@gmail.com>
2025-06-06 21:38:34 +0900

    feat: 임시 결제 정보 API Swagger 작성

---
+++ src/main/java/in/koreatech/payment/toss/controller/TossApi.java
@@ -0,0 +1,32 @@
+package in.koreatech.payment.toss.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import in.koreatech.payment.toss.dto.request.TemporaryPaymentRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@Tag(name = "(Normal) Toss: 토스 페이먼츠", description = "토스 페이먼츠 API를 관리한다.")
+@RequestMapping("/toss")
+public interface TossApi {
+
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
+        }
+    )
+    @Operation(summary = "임시 결제 정보를 저장한다")
+    @GetMapping
+    ResponseEntity<Void> saveTemporaryPayment(
+        @RequestBody @Valid TemporaryPaymentRequest request
+    );
+}
