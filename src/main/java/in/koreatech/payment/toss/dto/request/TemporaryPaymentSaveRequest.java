7999e7788e0723da66465385fd9b7b1ff8da1dee
신관규 <soundbar91@gmail.com>
2025-06-06 21:49:48 +0900

    fix: 요청 DTO 클래스 네이밍 변경

--- src/main/java/in/koreatech/payment/toss/dto/request/TemporaryPaymentSaveRequest.java
+++ src/main/java/in/koreatech/payment/toss/dto/request/TemporaryPaymentSaveRequest.java
@@ -11,7 +11,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @JsonNaming(value = SnakeCaseStrategy.class)
-public record TemporaryPaymentRequest(
+public record TemporaryPaymentSaveRequest(
     @Schema(description = "주문 번호", example = "a4CWyWY5m89PNh7xJwhk1", requiredMode = REQUIRED)
     @Size(min = 6, max = 64, message = "주문번호는 6자 이상, 64자 이하입니다.")
     @NotBlank(message = "주문 번호는 필수 입력사항입니다.")
