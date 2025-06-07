b82f1d3091482d9275a9ce425312ad4726542abc
신관규 <soundbar91@gmail.com>
2025-06-07 12:44:40 +0900

    refactor: TemporaryPayment 클래스 리펙토링

--- src/main/java/in/koreatech/payment/model/TemporaryPayment.java
+++ src/main/java/in/koreatech/payment/model/TemporaryPayment.java
@@ -11,7 +11,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -19,7 +21,16 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "temporary_payment")
+@Table(
+    schema = "koin_payment",
+    name = "temporary_payment",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_temporary_payment_order_id", columnNames = "order_id")
+    },
+    indexes = {
+        @Index(name = "idx_temporary_payment_order_id", columnList = "order_id")
+    }
+)
 @Where(clause = "is_deleted=0")
 @NoArgsConstructor(access = PROTECTED)
 public class TemporaryPayment extends BaseEntity {
@@ -33,11 +44,11 @@ public class TemporaryPayment extends BaseEntity {
 
     @NotNull
     @Size(min = 6, max = 64)
-    @Column(name = "order_id", nullable = false, updatable = false, unique = true, length = 64)
+    @Column(name = "order_id", nullable = false, updatable = false, length = 64)
     private String orderId;
 
     @NotNull
-    @Column(name = "usre_id", nullable = false, updatable = false)
+    @Column(name = "user_id", nullable = false, updatable = false)
     private Integer userId;
 
     @NotNull
@@ -45,7 +56,7 @@ public class TemporaryPayment extends BaseEntity {
     private Integer amount;
 
     @NotNull
-    @Column(name = "is_deleted", nullable = false)
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "TINYINT(1) NOT NULL DEFAULT 0")
     private Boolean isDeleted = FALSE;
 
     public TemporaryPayment(String orderId, Integer userId, Integer amount) {
