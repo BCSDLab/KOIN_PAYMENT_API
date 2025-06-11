02214e8e2a841f22ffc341dfdf990792b1e0929b
신관규 <soundbar91@gmail.com>
2025-06-11 10:00:13 +0900

    feat: WebConfig 클래스 추가

---
+++ src/main/java/in/koreatech/payment/config/WebConfig.java
@@ -0,0 +1,33 @@
+package in.koreatech.payment.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import in.koreatech.payment.common.auth.ExtractAuthenticationInterceptor;
+import in.koreatech.payment.common.auth.UserIdArgumentResolver;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ExtractAuthenticationInterceptor extractAuthenticationInterceptor;
+
+    private final UserIdArgumentResolver userIdArgumentResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(extractAuthenticationInterceptor)
+            .addPathPatterns("/**")
+            .order(0);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
+    }
+}
