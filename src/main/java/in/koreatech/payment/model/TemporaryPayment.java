a418ade7744237cf413a71ffe2460044880ecf32
신관규 <soundbar91@gmail.com>
2025-06-11 10:52:29 +0900

    fix: 레디스 객체로 변환

--- src/main/java/in/koreatech/payment/model/TemporaryPayment.java
+++ src/main/java/in/koreatech/payment/model/TemporaryPayment.java
@@ -1,69 +1,39 @@
 package in.koreatech.payment.model;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static java.lang.Boolean.FALSE;
-import static lombok.AccessLevel.PROTECTED;
+import java.time.LocalDateTime;
 
-import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
-import in.koreatech.payment.common.model.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Entity
-@Table(
-    schema = "koin_payment",
-    name = "temporary_payment",
-    uniqueConstraints = {
-        @UniqueConstraint(name = "uk_temporary_payment_order_id", columnNames = "order_id")
-    },
-    indexes = {
-        @Index(name = "idx_temporary_payment_order_id", columnList = "order_id")
-    }
-)
-@Where(clause = "is_deleted=0")
-@NoArgsConstructor(access = PROTECTED)
-public class TemporaryPayment extends BaseEntity {
+@RedisHash(value = "TemporaryPayment@")
+public class TemporaryPayment {
 
     private static final String ORDER_ID_PATTERN = "^[a-zA-Z0-9-_]{6,64}$";
+    private static final Long CACHE_EXPIRE_SECOND = 60 * 10L;
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "id", nullable = false, updatable = false)
-    private Integer id;
-
-    @NotNull
-    @Size(min = 6, max = 64)
-    @Column(name = "order_id", nullable = false, updatable = false, length = 64)
     private String orderId;
 
-    @NotNull
-    @Column(name = "user_id", nullable = false, updatable = false)
     private Integer userId;
 
-    @NotNull
-    @Column(name = "amount", nullable = false, updatable = false)
     private Integer amount;
 
-    @NotNull
-    @Column(name = "is_deleted", nullable = false, columnDefinition = "TINYINT(1) NOT NULL DEFAULT 0")
-    private Boolean isDeleted = FALSE;
+    @TimeToLive
+    private Long expiryTime;
+
+    private LocalDateTime createdAt;
 
     private TemporaryPayment(String orderId, Integer userId, Integer amount) {
         validateOrderIdPattern(orderId);
         this.orderId = orderId;
         this.userId = userId;
         this.amount = amount;
+        this.expiryTime = CACHE_EXPIRE_SECOND;
+        this.createdAt = LocalDateTime.now();
     }
 
     public static TemporaryPayment of(String orderId, Integer userId, Integer amount) {
