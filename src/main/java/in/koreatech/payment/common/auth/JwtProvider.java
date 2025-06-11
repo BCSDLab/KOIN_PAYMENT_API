fdcb0f2db537ce5b632a83a7c244afe58777882b
신관규 <soundbar91@gmail.com>
2025-06-11 09:50:01 +0900

    feat: 인증 클래스 추가

---
+++ src/main/java/in/koreatech/payment/common/auth/JwtProvider.java
@@ -0,0 +1,45 @@
+package in.koreatech.payment.common.auth;
+
+import java.util.Base64;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import in.koreatech.payment.common.auth.exception.AuthenticationException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtProvider {
+
+    private final String secretKey;
+
+    public JwtProvider(
+        @Value("${jwt.secret-key}") String secretKey
+    ) {
+        this.secretKey = secretKey;
+    }
+
+    public Integer getUserId(String token) {
+        try {
+            String userId = Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("id")
+                .toString();
+            return Integer.parseInt(userId);
+        } catch (JwtException e) {
+            throw AuthenticationException.withDetail("token: " + token);
+        }
+    }
+
+    private SecretKey getSecretKey() {
+        String encoded = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        return Keys.hmacShaKeyFor(encoded.getBytes());
+    }
+}
---
+++ src/main/java/in/koreatech/payment/common/auth/UserIdContext.java
@@ -0,0 +1,19 @@
+package in.koreatech.payment.common.auth;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class UserIdContext {
+
+    private Integer userId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
