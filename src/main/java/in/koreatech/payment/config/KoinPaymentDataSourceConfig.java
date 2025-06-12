098893d5f86705120c159ec2d75c95f563f5ec55
신관규 <soundbar91@gmail.com>
2025-06-12 17:29:14 +0900

    fix: 트랜잭션 primary 설정

--- src/main/java/in/koreatech/payment/config/KoinPaymentDataSourceConfig.java
+++ src/main/java/in/koreatech/payment/config/KoinPaymentDataSourceConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -45,6 +46,7 @@ public class KoinPaymentDataSourceConfig {
     }
 
     @Bean
+    @Primary
     public PlatformTransactionManager koinPaymentTransactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(koinPaymentEntityManagerFactory().getObject());
