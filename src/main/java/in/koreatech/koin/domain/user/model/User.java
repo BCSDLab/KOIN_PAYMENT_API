e9bdd3fb54f3b809c2158119a4574746df742fce
신관규 <soundbar91@gmail.com>
2025-06-14 22:53:55 +0900

    fix: user 클래스 축소

--- src/main/java/in/koreatech/koin/domain/user/model/User.java
+++ src/main/java/in/koreatech/koin/domain/user/model/User.java
@@ -25,61 +25,10 @@ import lombok.NoArgsConstructor;
 @Table(schema = "koin", name = "users")
 @Where(clause = "is_deleted=0")
 @NoArgsConstructor(access = PROTECTED)
-public class User extends BaseEntity {
+public class User {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Integer id;
-
-    @Size(max = 50)
-    @Column(name = "name", length = 50)
-    private String name;
-
-    @Size(max = 50)
-    @Column(name = "nickname", length = 50, unique = true)
-    private String nickname;
-
-    @Size(max = 20)
-    @Column(name = "phone_number", length = 20)
-    private String phoneNumber;
-
-    @Size(max = 100)
-    @Column(name = "email", length = 100)
-    private String email;
-
-    @Size(max = 255)
-    @Column(name = "profile_image_url")
-    private String profileImageUrl;
-
-    @NotNull
-    @Column(name = "user_id", nullable = false)
-    private String loginId;
-
-    @NotNull
-    @Column(name = "password", nullable = false)
-    private String loginPw;
-
-    @Column(name = "device_token")
-    private String deviceToken;
-
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    @Column(name = "user_type", nullable = false, length = 20)
-    private UserType userType;
-
-    @Column(name = "gender", columnDefinition = "INT")
-    @Enumerated(value = EnumType.ORDINAL)
-    private UserGender gender;
-
-    @NotNull
-    @Column(name = "is_authed", nullable = false)
-    private boolean isAuthed = false;
-
-    @NotNull
-    @Column(name = "is_deleted", nullable = false)
-    private boolean isDeleted = false;
-
-    @Column(name = "last_logged_at", columnDefinition = "TIMESTAMP")
-    private LocalDateTime lastLoggedAt;
 
 }
