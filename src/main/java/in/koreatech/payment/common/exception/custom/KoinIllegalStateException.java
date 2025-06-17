e0a3c8fd47f1e745de8e9393a7bde37d3124a9c1
신관규 <soundbar91@gmail.com>
2025-06-17 10:14:42 +0900

    chore: 상수 변수명 변경

--- src/main/java/in/koreatech/payment/common/exception/custom/KoinIllegalStateException.java
+++ src/main/java/in/koreatech/payment/common/exception/custom/KoinIllegalStateException.java
@@ -3,14 +3,14 @@ package in.koreatech.payment.common.exception.custom;
 public class KoinIllegalStateException extends KoinException {
 
     private static final String DEFAULT_MESSAGE = "서버에 문제가 생겼습니다.";
-    private static final String code = "KOIN_SERVER_ERROR";
+    private static final String CODE = "KOIN_SERVER_ERROR";
 
     public KoinIllegalStateException(String message) {
-        super(message, code);
+        super(message, CODE);
     }
 
     public KoinIllegalStateException(String message, String detail) {
-        super(message, detail, code);
+        super(message, detail, CODE);
     }
 
     public static KoinIllegalStateException withDetail(String detail) {
