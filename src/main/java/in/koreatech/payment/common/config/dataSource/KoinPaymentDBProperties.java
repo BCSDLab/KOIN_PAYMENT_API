1c8fd21b4a6ede982c92c287cb4c1b0c064a6083
신관규 <soundbar91@gmail.com>
2025-06-20 10:45:09 +0900

    fix: KoinPaymentDataSourceConfig 수정

---
+++ src/main/java/in/koreatech/payment/common/config/dataSource/KoinPaymentDBProperties.java
@@ -0,0 +1,11 @@
+package in.koreatech.payment.common.config.dataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.datasource.koin-payment.hibernate")
+public record KoinPaymentDBProperties(
+    String ddlAuto,
+    Boolean showSql,
+    String packagesToScan
+) {
+}
