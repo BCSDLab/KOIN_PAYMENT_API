77d452522f095ad594316988598bb990ef55b520
신관규 <soundbar91@gmail.com>
2025-06-23 23:59:10 +0900

    fix: 결제 승인 API 주석 처리 삭제

--- src/main/java/in/koreatech/payment/controller/PaymentsApi.java
+++ src/main/java/in/koreatech/payment/controller/PaymentsApi.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.koreatech.payment.common.auth.AccessToken;
 import in.koreatech.payment.dto.request.PaymentCancelRequest;
+import in.koreatech.payment.dto.request.PaymentConfirmRequest;
 import in.koreatech.payment.dto.request.TemporaryDeliveryPaymentSaveRequest;
 import in.koreatech.payment.dto.request.TemporaryTakeoutPaymentSaveRequest;
 import in.koreatech.payment.dto.response.PaymentCancelResponse;
+import in.koreatech.payment.dto.response.PaymentConfirmResponse;
 import in.koreatech.payment.dto.response.TemporaryPaymentResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -41,15 +43,15 @@ public interface PaymentsApi {
         @AccessToken final String accessToken
     );
 
-    // @Operation(
-    //     summary = "결제 승인을 한다.",
-    //     description = "결제 승인을 한다."
-    // )
-    // @PostMapping("/confirm")
-    // ResponseEntity<PaymentConfirmResponse> confirmPayment(
-    //     @RequestBody @Valid final PaymentConfirmRequest request,
-    //     @AccessToken final String accessToken
-    // );
+    @Operation(
+        summary = "결제 승인을 한다.",
+        description = "결제 승인을 한다."
+    )
+    @PostMapping("/confirm")
+    ResponseEntity<PaymentConfirmResponse> confirmPayment(
+        @RequestBody @Valid final PaymentConfirmRequest request,
+        @AccessToken final String accessToken
+    );
 
     @Operation(
         summary = "결제 취소를 한다.",
