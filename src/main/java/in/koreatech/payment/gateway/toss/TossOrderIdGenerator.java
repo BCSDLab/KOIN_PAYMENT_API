8e4a843fd329ea4f83204a8473ec0f4fcc864adb
신관규 <soundbar91@gmail.com>
2025-08-26 21:09:19 +0900

    chore: TossOrderIdGenerator 패키지 이동

--- src/main/java/in/koreatech/payment/gateway/toss/TossOrderIdGenerator.java
+++ src/main/java/in/koreatech/payment/gateway/toss/TossOrderIdGenerator.java
@@ -1,4 +1,4 @@
-package in.koreatech.payment.util;
+package in.koreatech.payment.gateway.toss;
 
 import java.security.SecureRandom;
 
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import in.koreatech.payment.gateway.pg.PgOrderIdGenerator;
 
 @Component
-public class TossPaymentOrderIdGenerator implements PgOrderIdGenerator {
+public class TossOrderIdGenerator implements PgOrderIdGenerator {
 
     private static final String ORDER_ID_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
     private static final int ORDER_ID_MIN_LENGTH = 6;
