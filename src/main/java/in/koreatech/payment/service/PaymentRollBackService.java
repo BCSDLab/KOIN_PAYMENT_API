9f01554d4a5a636b876a8a5c7450ba0e163c40e7
신관규 <soundbar91@gmail.com>
2025-08-27 21:53:18 +0900

    fix: PaymentRollBackService 인터페이스 삭제

--- src/main/java/in/koreatech/payment/service/PaymentRollBackService.java
+++
@@ -1,7 +0,0 @@
-package in.koreatech.payment.service;
-
-import in.koreatech.payment.event.TossPaymentRollBackEvent;
-
-public interface PaymentRollBackService {
-    void paymentRollback(TossPaymentRollBackEvent event);
-}
