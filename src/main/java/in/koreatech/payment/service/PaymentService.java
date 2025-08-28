384c9c4f01f25329a179435941c4aabe376e81a3
신관규 <soundbar91@gmail.com>
2025-08-28 20:41:54 +0900

    refactor: PaymentService 결제 취소 로직 리펙토링

--- src/main/java/in/koreatech/payment/service/PaymentService.java
+++ src/main/java/in/koreatech/payment/service/PaymentService.java
@@ -8,10 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import in.koreatech.koin.domain.order.model.Order;
 import in.koreatech.koin.domain.order.model.OrderMenu;
 import in.koreatech.koin.domain.order.model.Payment;
-import in.koreatech.koin.domain.order.model.PaymentCancel;
-import in.koreatech.koin.domain.order.model.PaymentStatus;
 import in.koreatech.koin.domain.order.repository.OrderMenuRepository;
-import in.koreatech.koin.domain.order.repository.PaymentCancelRepository;
 import in.koreatech.koin.domain.order.repository.PaymentRepository;
 import in.koreatech.koin.domain.user.model.User;
 import in.koreatech.koin.domain.user.repository.UserRepository;
@@ -22,11 +19,6 @@ import in.koreatech.payment.dto.response.PaymentCancelResponse;
 import in.koreatech.payment.dto.response.PaymentConfirmResponse;
 import in.koreatech.payment.dto.response.PaymentResponse;
 import in.koreatech.payment.dto.response.TemporaryPaymentResponse;
-import in.koreatech.payment.exception.PaymentAlreadyCanceledException;
-import in.koreatech.payment.exception.PaymentCancelException;
-import in.koreatech.payment.gateway.pg.PaymentGatewayService;
-import in.koreatech.payment.gateway.pg.dto.PgPaymentCancelResponse;
-import in.koreatech.payment.mapper.PaymentCancelMapper;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -37,13 +29,10 @@ public class PaymentService {
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
     private final PaymentRepository paymentRepository;
-    private final PaymentIdempotencyKeyService paymentIdempotencyKeyService;
-    private final PaymentCancelRepository paymentCancelRepository;
     private final OrderMenuRepository orderMenuRepository;
-    private final PaymentGatewayService paymentGatewayService;
-    private final PaymentCancelMapper paymentCancelMappers;
     private final TemporaryPaymentService temporaryPaymentService;
     private final PaymentConfirmService paymentConfirmService;
+    private final PaymentCancelService paymentCancelService;
 
     @Transactional
     public TemporaryPaymentResponse createTemporaryDeliveryPayment(String accessToken, TemporaryDeliveryPaymentSaveRequest request) {
@@ -70,23 +59,7 @@ public class PaymentService {
     public PaymentCancelResponse cancelPayment(String accessToken, Integer paymentId, String cancelReason) {
         Integer userId = jwtProvider.getUserId(accessToken);
         User user = userRepository.getById(userId);
-        Payment payment = paymentRepository.getById(paymentId);
-        if (payment.getPaymentStatus().isCanceled()) {
-            throw PaymentAlreadyCanceledException.withDetail("paymentId : " + payment.getId());
-        }
-        payment.validateUserIdMatches(user.getId());
-
-        String paymentIdempotencyKey = paymentIdempotencyKeyService.getOrCreate(user.getId());
-        PgPaymentCancelResponse pgPaymentCancelResponse = paymentGatewayService.cancelPayment(payment.getPaymentKey(), cancelReason, paymentIdempotencyKey);
-        if (!PaymentStatus.valueOf(pgPaymentCancelResponse.status()).isCanceled()) {
-            throw PaymentCancelException.withDetail("paymentStatus : " + pgPaymentCancelResponse.status());
-        }
-
-        payment.cancel();
-
-        List<PaymentCancel> paymentCancels = paymentCancelMappers.toEntity(payment, pgPaymentCancelResponse);
-        paymentCancelRepository.saveAll(paymentCancels);
-        return PaymentCancelResponse.from(paymentCancels);
+        return paymentCancelService.cancelPayment(user, paymentId, cancelReason);
     }
 
     public PaymentResponse getPayment(String accessToken, Integer paymentId) {
